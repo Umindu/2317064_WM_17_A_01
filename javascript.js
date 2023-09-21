7f13192a637bac280685eefd4efc81970531dd08
Umindu <ybumindu@gmail.com>
2023-09-21 09:46:13 +0530

    4

--- javascript.js
+++ javascript.js
@@ -2,18 +2,26 @@ function ShowHide() {
     var show = document.getElementById("show");
     var hide = document.getElementById("hide");
 
+    var password = document.getElementById("password");
+
     if (show.style.display === "block") {
         show.style.display = "none";
         hide.style.display = "block";
         hide.style.border = "2px solid #0A66C2";
         hide.style.background = "#0a66c21f";
+        // password
+        password.type = "text";
+
     } else {
         hide.style.display = "none";
         show.style.display = "block";
         show.style.border = "2px solid #0A66C2";
         show.style.background = "#0a66c21f";
+        // text
+        password.type = "password";
     }
 }
+
 
 
 document.addEventListener('DOMContentLoaded', function () {
